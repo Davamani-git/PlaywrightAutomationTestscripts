0c91e7ab8aad6f0a9a68845087f59e62751a2014
Davamani-git <davamani.a@ascendion.com>
2026-02-11 13:35:56 +0530

    testing multiple commit

---
+++ Automation/src/actions/CartActions.ts
@@ -0,0 +1,3 @@
+import { CartPage } from '../pages/CartPage';
+import { Page } from '@playwright/test';
+export class CartActions { readonly page: Page; constructor(page: Page) { this.page = page; } }
